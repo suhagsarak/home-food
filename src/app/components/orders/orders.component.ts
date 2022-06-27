cd7631c262e27cac27b93bed79eb51b2b23b05a7
suhag_sarak <suhag.sarak@phenompeople.com>
2022-06-27 23:16:24 +0530

    orders page done

--- src/app/components/orders/orders.component.ts
+++ src/app/components/orders/orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { LocalStorageService } from 'src/app/service/local-storage.service';
 import { UtilsService } from 'src/app/service/utils.service';
 
@@ -10,6 +11,8 @@ import { UtilsService } from 'src/app/service/utils.service';
 export class OrdersComponent implements OnInit {
 
   userOrder: Array<any> = [];
+  isOrderComplete: boolean;
+  totalPrice: number;
 
   constructor(
     private localStorageService: LocalStorageService,
@@ -23,20 +26,29 @@ export class OrdersComponent implements OnInit {
       counts[orde.pid] = (counts[orde.pid] || 0) + 1;
     });
     let rating, prod;
+    this.totalPrice = 0
+    this.userOrder = [];
     Object.keys(counts).forEach((pid) => {
       prod = products.find((o) => o.pid == pid);
       this.userOrder.push({ ...prod, count: counts[pid] });
     });
-    this.userOrder.map((ord) => {
+    this.userOrder = this.userOrder.map((ord) => {
       rating = this.utilsService.getRating();
+      this.totalPrice += ord.count * ord.price;
       return {
         ...ord,
         pName: this.utilsService.getItemName(ord.name),
         rating,
         ratPerc: (rating / 5) * 100,
-        src: `/assets/image/${ord.category}/${ord.name}`
+        src: `/assets/image/${ord.category}/${ord.name}`,
+        totalPrice: (ord.count * ord.price)
       }
     });
+  }
+
+  checkOut() {
+    // API
+    this.isOrderComplete = true;
   }
 
 }
